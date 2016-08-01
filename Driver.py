f21229d721f791f979f8bcd0052459371a82548c
dragon <divyanshu_j01@yahoo.in>
2016-08-01 09:17:47 +0530

    Create Driver.py

---
+++ Driver.py
@@ -0,0 +1,21 @@
+from Matrix import Matrix
+
+m1 = Matrix()
+m2 = Matrix()
+
+m1.inputMatrix()
+m2.inputMatrix()
+
+#m = m1.addMatrix(m2)
+#m3 = m1.subMatrix(m2)
+#m.printMatrix()
+#m3.printMatrix()
+
+#m1.transMatrix()
+#m1.printMatrix()
+
+#print(m1.detMatrix())
+
+#m = m1.mulMatrix(m2)
+
+#m.printMatrix()
