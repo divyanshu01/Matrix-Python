f6ec295974c0c000935262bf47a610fb24a1bdd5
dragon <divyanshu_j01@yahoo.in>
2016-08-04 23:54:08 +0530

    Update Matrix.py

--- Matrix.py
+++ Matrix.py
@@ -125,5 +125,28 @@ class Matrix:
 					self.matrix_list[i][j], self.matrix_list[j][i] = self.matrix_list[j][i], self.matrix_list[i][j]
 	
 	
+	def getMinorForCofactor(self, matrix_list, i, j):
+		#print("Entry:", matrix_list)
+		matrix = copy.deepcopy(matrix_list)
+		del matrix[i]
+		for k in range(len(matrix)):
+			del matrix[k][j]
+		#print("After deletions", matrix_list)
+		
+		m = Matrix()
+		m.matrix_list = matrix[:]
+		m.no_of_row = len(m.matrix_list)
+		m.no_of_col = len(m.matrix_list[0])
+		x = m.detMatrix()
+		#print(m.matrix_list, m.no_of_row, m.no_of_col)
+		return x
 	
-	
+	def getCofactorMatrix(self):
+		if self.no_of_row == 2 and self.no_of_col == 2:
+			return self.matrix_list[0][0] * self.matrix_list[1][1] - self.matrix_list[0][1] * self.matrix_list[1][0]
+		else:
+			det = 0
+			for i in range(self.no_of_row):
+				for j in range(self.no_of_col):
+					det += ((-1)**(i + j)) * self.getMinorForCofactor(self.matrix_list, i, j);
+			return det
